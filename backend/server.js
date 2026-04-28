2e7fa0b64be067db7c0d854a60dd88d2095cb27c
Mauli Kukreti <maulikukreti07@gmail.com>
2026-04-28 07:31:16 +0530

    fixed backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');

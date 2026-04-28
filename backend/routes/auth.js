333dcd6f96f7bc86c72678e18afb5db51d9b1898
Mauli Kukreti <maulikukreti07@gmail.com>
2026-04-28 07:08:30 +0530

    fix case-sensitive import issue

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 const auth = require('../middleware/authMiddleware');
 
 router.post('/register', async (req, res) => {
